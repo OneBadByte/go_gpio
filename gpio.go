0c92e5d9491bd33d041b6689b2ebf82a72cefc48
blackdartq <brody.prestwich18@gmail.com>
2017-02-12 20:15:24 +0000

    adding stuff

--- gpio.go
+++ gpio.go
@@ -1,10 +1,38 @@
 package go_gpio
 
-import "fmt"
 
-func Test(){
+import(
 
-	fmt.Println("testing")
+	"io/ioutil"
+	//"fmt"
+	"strconv"
+      )
+
+func    ChangeGpioValue(gpioPin int, gpioValue int){
+	gpio := strconv.Itoa(gpioPin)
+	var filePath string = "/sys/class/gpio/gpio"+ gpio + "/value"
+	gpioVal := strconv.Itoa(gpioValue)
+	ioutil.WriteFile(filePath, []byte(gpioVal), 777)
+}
+
+func ReadGpioValue(gpioPin int) string{
+	gpio := strconv.Itoa(gpioPin)
+	var filePath string = "/sys/class/gpio/gpio"+ gpio + "/value"
+	value, err := ioutil.ReadFile(filePath)
+	if err != nil{
+		return "-1"
+	}
+	return string(value)
 	
 
+}
+
+func ExportGpio(gpioPin int, gpioDirection string){
+
+	gpio := strconv.Itoa(gpioPin)
+	var filePath string = "/sys/class/gpio/export"
+	var directionPath string = "/sys/class/gpio/gpio" + gpio + "/direction"
+	ioutil.WriteFile(filePath, []byte(gpio), 777)
+	ioutil.WriteFile(directionPath, []byte(gpioDirection), 777)
+	
 }
